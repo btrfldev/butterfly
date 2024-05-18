2357a1d332a76616196c7c0a41468affbef4e30f
Soloma <soloma@tinaj.ru>
2024-05-18 18:29:46 +0500

    Now it works with [] strings.

--- dust/main.go
+++ dust/main.go
@@ -22,7 +22,7 @@ func main() {
 	}
 	bodyLimit, err := strconv.Atoi(os.Getenv("BODYLIMIT"))
 	if err != nil {
-		bodyLimit = 1024*1024*1024*1024
+		bodyLimit = 1024 * 1024 * 1024 * 1024
 		fmt.Println("Can`t parse BodyLimit! Used standard: 1024*1024*1024*1024.")
 	}
 	s := NewServer(":"+port, bodyLimit, time.Duration(timeout))
@@ -50,98 +50,129 @@ func (s *Server) Start() error {
 func (s *Server) Put(c *fiber.Ctx) error {
 	query := Query{}
 
+	//parse query
 	if err := c.BodyParser(&query); err != nil {
 		return c.Status(http.StatusBadRequest).Send([]byte("Can`t parse JSON!"))
 	}
 
-	if query.Lib == "" || query.Key == "" {
-		return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty!"))
-	}
-	
-	if strings.Contains(query.Lib, ":") || strings.Contains(query.Key, ":") {
-		return c.Status(http.StatusBadRequest).Send([]byte("You can`t use ':' in Lib or Key"))
+	//check all objects
+	for i, obj := range query.Objects {
+		if obj.Lib == "" || obj.Key == "" {
+			return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty! Object " + fmt.Sprint(i+1)))
+		}
+
+		if strings.Contains(obj.Lib, ":") || strings.Contains(obj.Key, ":") {
+			return c.Status(http.StatusBadRequest).Send([]byte("You can`t use ':' in Lib or Key! Object " + fmt.Sprint(i+1)))
+		}
 	}
 
-	if err := s.Dust.Put(query.Lib+":"+query.Key, query.Value); err != nil {
-		return c.Status(http.StatusInternalServerError).Send([]byte(err.Error()))
-	} else {
-		return c.JSON(map[string]string{"status": "ok"})
+	//put all objects
+	for i, obj := range query.Objects {
+		if err := s.Dust.Put(obj.Lib+":"+obj.Key, obj.Value); err != nil {
+			return c.Status(http.StatusInternalServerError).Send([]byte(err.Error() + " Object " + fmt.Sprint(i+1)))
+		}
 	}
+	return c.JSON(map[string]string{"status": "ok"})
 }
 
 func (s *Server) Get(c *fiber.Ctx) (err error) {
 	query := Query{}
+
+	//parse query
 	if err = c.BodyParser(&query); err != nil {
 		return c.Status(http.StatusBadRequest).Send([]byte("Can`t parse JSON!"))
 	}
 
-	if query.Lib == "" || query.Key == "" {
-		return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty!"))
+	//check all objects
+	for i, obj := range query.Objects {
+		if obj.Lib == "" || obj.Key == "" {
+			return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty! Object " + fmt.Sprint(i+1)))
+		}
 	}
 
-	if query.Value, err = s.Dust.Get(query.Lib + ":" + query.Key); err != nil {
-		return c.Status(http.StatusInternalServerError).Send([]byte(err.Error()))
-	} else {
-		return c.JSON(map[string]string{"value": query.Value})
+	//get all objects
+	for i, obj := range query.Objects {
+		if query.Objects[i].Value, err = s.Dust.Get(obj.Lib + ":" + obj.Key); err != nil {
+			return c.Status(http.StatusInternalServerError).Send([]byte(err.Error() + " Object " + fmt.Sprint(i+1)))
+		}
 	}
+	return c.JSON(query)
 }
 
 func (s *Server) Update(c *fiber.Ctx) (err error) {
 	query := Query{}
 
+	//parse query
 	if err = c.BodyParser(&query); err != nil {
 		return c.Status(http.StatusBadRequest).Send([]byte("Can`t parse JSON!"))
 	}
 
-	if query.Lib == "" || query.Key == "" {
-		return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty!"))
+	//check all objects
+	for i, obj := range query.Objects {
+		if obj.Lib == "" || obj.Key == "" {
+			return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty!" + " Object " + fmt.Sprint(i+1)))
+		}
 	}
 
-	if err := s.Dust.Update(query.Lib+":"+query.Key, query.Value); err != nil {
-		return c.Status(http.StatusInternalServerError).Send([]byte(err.Error()))
-	} else {
-		return c.JSON(map[string]string{"status": "ok"})
+	//update all objects
+	for i, obj := range query.Objects {
+		if err := s.Dust.Update(obj.Lib+":"+obj.Key, obj.Value); err != nil {
+			return c.Status(http.StatusInternalServerError).Send([]byte(err.Error() + " Object " + fmt.Sprint(i+1)))
+		}
 	}
+	return c.JSON(map[string]string{"status": "ok"})
 }
 
 func (s *Server) Delete(c *fiber.Ctx) (err error) {
 	query := Query{}
 
+	//parse query
 	if err = c.BodyParser(&query); err != nil {
 		return c.Status(http.StatusBadRequest).Send([]byte("Can`t parse JSON!"))
 	}
 
-	if query.Lib == "" || query.Key == "" {
-		return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty!"))
+	//check all objects
+	for i, obj := range query.Objects {
+		if obj.Lib == "" || obj.Key == "" {
+			return c.Status(http.StatusBadRequest).Send([]byte("Lib or Key is empty!" + " Object " + fmt.Sprint(i+1)))
+		}
 	}
 
-	if value, err := s.Dust.Delete(query.Lib + ":" + query.Key); err != nil {
-		return c.Status(http.StatusInternalServerError).Send([]byte(err.Error()))
-	} else {
-		return c.JSON(map[string]string{"status": "ok", "value": value})
+	//delete all objects
+	for i, obj := range query.Objects {
+		if query.Objects[i].Value, err = s.Dust.Delete(obj.Lib + ":" + obj.Key); err != nil {
+			return c.Status(http.StatusInternalServerError).Send([]byte(err.Error() + " Object " + fmt.Sprint(i+1)))
+		}
 	}
+	return c.JSON(query)
 }
 
 func (s *Server) List(c *fiber.Ctx) (err error) {
 	query := Query{}
 	resp := ListResp{
-		Count: 0,
-		Keys:  []string{},
+		Lists: []List{},
 	}
 
+	//parce query
 	if err = c.BodyParser(&query); err != nil {
 		return c.Status(http.StatusBadRequest).Send([]byte("Can`t parse JSON!"))
 	}
 
-	if keys, err := s.Dust.List(); err!=nil{
+	//get lists
+	if keys, err := s.Dust.List(); err != nil {
 		return c.Status(http.StatusInternalServerError).Send([]byte(err.Error()))
 	} else {
-		for _, key := range keys {
-			if strings.HasPrefix(key, query.Lib+":"+query.Key) {
-				resp.Count += 1
-				resp.Keys = append(resp.Keys, key)
+		//sort by prefix
+		for i, obj := range query.Objects {
+			resp.Lists = append(resp.Lists, List{Prefix: obj.Lib + ":" + obj.Key})
+			for _, key := range keys {
+				if strings.HasPrefix(key, obj.Lib+":"+obj.Key) {
+					resp.Lists[i].Count += 1
+					resp.Lists[i].Keys = append(resp.Lists[i].Keys, key)
+				}
 			}
 		}
+
 		return c.JSON(resp)
 	}
 }
@@ -150,10 +181,10 @@ func (s *Server) Health(c *fiber.Ctx) (err error) {
 	memory := system.ReadMemoryStats()
 
 	resp := Health{
-		Status:       "ok",
-		UTC:          time.Now().UTC().String(),
-		StorerType:   "Dust",
-		TotalStorage:  memory.MemTotal,
+		Status:           "ok",
+		UTC:              time.Now().UTC().String(),
+		StorerType:       "Dust",
+		TotalStorage:     memory.MemTotal,
 		AvailableStorage: memory.MemAvailable,
 	}
 	return c.JSON(resp)
--- dust/types.go
+++ dust/types.go
@@ -7,12 +7,21 @@ import (
 )
 
 type Query struct {
+	Objects []Object `json:"objects"`
+}
+
+type Object struct {
 	Lib   string `json:"lib"`
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
 
 type ListResp struct {
+	Lists []List `json:"keysLists"`
+}
+
+type List struct {
+	Prefix string `json:"prefix"`
 	Count uint64 `json:"count"`
 	Keys []string `json:"libs:keys"`
 }
