b2edb29b101a8528be53e1f93721ed531a30ebd7
Soloma <soloma@tinaj.ru>
2024-05-27 00:30:18 +0500

    some work for flower

--- FileInteraction.go
+++ FileInteraction.go
@@ -6,12 +6,14 @@ import (
 	"errors"
 	"io"
 	"os"
+	"strings"
 )
 
 //			  1   2  3  4  5   6
-//1 string = key:key:-:key:-:key
-//             3     5
-//2 string = trash:trash
+//1 string = key;key;-;key;-;key;
+//               3          5
+//2 string = numoftrash:numoftrash
+//3 string = user:password;-:-;user:password;
 //[6; infinitive] = value
 // 					value
 //					  -
@@ -19,20 +21,36 @@ import (
 //					  -
 //					value
 
-func LineCounter(fullpath string) (int, error) {
+func GetKeySpace(fullpath string) (KeySpace []string, err error) {
+	file, err := os.OpenFile(fullpath, os.O_RDONLY, 0600)
+	if err != nil {
+		return nil, errors.New("Can`t open the file while getting Key Space: " + err.Error())
+	}
+
+	defer file.Close()
+	kspstr, _, err := GetLineByNum(file, 0)
+	if err != nil {
+		return nil, errors.New("Can`t get the line whith a Key Space: " + err.Error())
+	}
+
+	KeySpace = strings.Split(string(kspstr), ";")
+	return KeySpace, nil
+}
+
+func LineCounter(r io.Reader /*fullpath string*/) (int, error) {
 	buf := make([]byte, 1*1024) //1 Kbyte
 	count := 0
 	lineSep := []byte{'\n'}
 
-	file, err := os.OpenFile(fullpath, os.O_RDONLY, 0600)
+	/*file, err := os.OpenFile(fullpath, os.O_RDONLY, 0600)
 	if err != nil {
 		return count, errors.New("Error occurred when determining the last line of the file: " + err.Error())
 	}
 
-	defer file.Close()
+	defer file.Close()*/
 
 	for {
-		c, err := file.Read(buf)
+		c, err := r.Read(buf) //file.Read(buf)
 		count += bytes.Count(buf[:c], lineSep)
 
 		switch {
